590caad53c2a45ab869fd833684b456b445b0684
Nishant <eddienishant@gmail.com>
2023-02-03 00:39:00 -0500

    test feature of homepage

--- cypress-realworld-testing-course-app-start/cypress/e2e/home.cy.ts
+++ cypress-realworld-testing-course-app-start/cypress/e2e/home.cy.ts
@@ -2,7 +2,15 @@
 describe("home page", () => {
   it("the h1 contains the correct text", () => {
     cy.visit("http://localhost:3000")
-    cy.get("h1").contains("Testing Next.js Applications with Cypress")
+
+    cy.get("[data-test='hero-heading']").contains("Testing Next.js Applications with Cypress")
+  })
+
+  it.only("the features of homepage are correact", () => {
+    cy.visit("http://localhost:3000")
+    cy.get("dt").eq(0).contains("4 Courses");
+    cy.get("dt").eq(1).contains("25+ Lessons");
+    cy.get("dt").eq(2).contains("Free and Open Source");
   })
 })
 
