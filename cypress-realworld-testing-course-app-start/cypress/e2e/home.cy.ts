5a950188ed482132af72aa831af4fbecfe93fc08
Nishant <eddienishant@gmail.com>
2023-02-03 00:13:06 -0500

    add simple command chaining for localhost

--- cypress-realworld-testing-course-app-start/cypress/e2e/home.cy.ts
+++ cypress-realworld-testing-course-app-start/cypress/e2e/home.cy.ts
@@ -1,5 +1,18 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+
+describe("home page", () => {
+  it("the h1 contains the correct text", () => {
+    cy.visit("http://localhost:3000")
+    cy.get("h1").contains("Testing Next.js Applications with Cypress")
   })
 })
+
+
+
+/*
+ - describe function takes 2 arguments, 1) a string which is a description of the test, 2nd callback function
+ - It ==< same as the describe function
+- visit is a command that tells Cypress where to execute our tests cy.visit('https://example.cypress.io')
+
+
+
+ */
