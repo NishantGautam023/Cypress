067b76e2f91916bf66c2ba5ef228cc745de94ede
Nishant <eddienishant@gmail.com>
2023-02-03 00:47:50 -0500

    using beforeEach Hook and command chaining

--- cypress-realworld-testing-course-app-start/cypress/e2e/home.cy.ts
+++ cypress-realworld-testing-course-app-start/cypress/e2e/home.cy.ts
@@ -1,26 +1,14 @@
-
-describe("home page", () => {
-  it("the h1 contains the correct text", () => {
-    cy.visit("http://localhost:3000")
-
-    cy.get("[data-test='hero-heading']").contains("Testing Next.js Applications with Cypress")
+describe("Home Page", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
   })
+  it("the h1 contains correct text", () => {
+    cy.get("[data-test='hero-heading']").contains("Testing Next.js Applications with Cypress");
 
-  it.only("the features of homepage are correact", () => {
-    cy.visit("http://localhost:3000")
-    cy.get("dt").eq(0).contains("4 Courses");
-    cy.get("dt").eq(1).contains("25+ Lessons");
-    cy.get("dt").eq(2).contains("Free and Open Source");
+  })
+  it("the features of home page are correact", () => {
+    cy.get("dt").eq(0).contains("4 Courses")
+    cy.get("dt").eq(1).contains("25+ Lessons")
+    cy.get("dt").eq(2).contains("Free and Open Source")
   })
 })
-
-
-
-/*
- - describe function takes 2 arguments, 1) a string which is a description of the test, 2nd callback function
- - It ==< same as the describe function
-- visit is a command that tells Cypress where to execute our tests cy.visit('https://example.cypress.io')
-
-
-
- */
